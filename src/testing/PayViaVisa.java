fc61a517cb04aa5facb26bb293690d15488b5b5c
Chingiz <ch.mambetaliev@gmail.com>
2023-04-05 15:37:40 +0600

    some changes

--- src/testing/PayViaVisa.java
+++ src/testing/PayViaVisa.java
@@ -3,7 +3,10 @@ package testing;
 public class PayViaVisa {
 
         public void payViaVisa(){
+
             System.out.println("Pay via Visa");
+            System.out.println("This is some changes");
         }
+
     }
 
