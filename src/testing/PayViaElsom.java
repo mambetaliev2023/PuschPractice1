f087e6a10f846bf44f1c17810ed584598aa5bf02
Chingiz <ch.mambetaliev@gmail.com>
2023-04-05 16:31:35 +0600

    feature/GCPINT3344

---
+++ src/testing/PayViaElsom.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class PayViaElsom {
+
+    public void payViaElsom() {
+
+
+        System.out.println("Pay via Visa");
+        System.out.println("This is some changes");
+    }
+}
